b5cd3976a964a320206c994110515c733369f3c7
Carl Owens <carl@partfire.co.uk>
2016-11-14 23:00:58 +0000

    add data fixtures for world countries

---
+++ Data/CountryData.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by Carl Owens (carl@partfire.co.uk)
+ * Company: PartFire Ltd (www.partfire.co.uk)
+ * Copyright © 2016 PartFire Ltd. All rights reserved.
+ *
+ * User:    Carl Owens
+ * Date:    14/11/2016
+ * Time:    21:59
+ * File:    CountryData.php
+ **/
+
+namespace Partfire\CommonBundle\Data;
+
+
+class CountryData
+{
+    protected $data;
+
+    /**
+     * @return mixed
+     */
+    public function getData()
+    {
+        $string = file_get_contents( __DIR__ . DIRECTORY_SEPARATOR . "CountryData.json");
+        return json_decode($string, false);
+    }
+}
