8b8a56bd134cc75d494fd72a8c448349a0846da4
Carl Owens <carl@partfire.co.uk>
2016-12-12 21:55:21 +0000

    add methjod to generate string

--- Entity/CommonBaseEntity.php
+++ Entity/CommonBaseEntity.php
@@ -239,4 +239,14 @@ class CommonBaseEntity
     {
         $this->updatedDate = $updatedDate;
     }
+
+    protected function generateRandomString($length = 10)
+    {
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $randomString = '';
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[rand(0, strlen($characters) - 1)];
+        }
+        return $randomString;
+    }
 }
